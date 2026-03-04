3bc60f93eb02cdb2b12072f5b368670da1bf1bfb
nicutiecus <100819560+nicutiecus@users.noreply.github.com>
2026-03-04 18:04:34 +0100

    Set DEBUG to False and update ALLOWED_HOSTS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s(4%^)gy)gl(+)stqr%_drut0_q#z(i_-lgrxpy6fvf&zf=10^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []#[os.environ.get('ALLOWED_HOSTS') ]
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS') ]
 
 
 # Application definition
