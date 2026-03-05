7b746fdb91bf2eae531aaa77549f599a29699ef9
Nikson Kejeh <niksonkejeh@gmail.com>
2026-03-05 15:34:35 +0100

    modified admin layout

--- frontend/src/layouts/AdminLayout.tsx
+++ frontend/src/layouts/AdminLayout.tsx
@@ -18,8 +18,10 @@ const AdminLayout: React.FC = () => {
         </div>
         <nav className="flex-1 p-4 flex flex-col gap-2">
           <Link to="/admin" className="p-2 hover:bg-gray-800 rounded">Overview</Link>
+          <Link to="/POS" className="p-2 hover:bg-gray-800 rounded">POS</Link>
           <Link to="/admin/categories" className="p-2 hover:bg-gray-800 rounded">Product Categories</Link>
           <Link to="/admin/inventory" className="p-2 hover:bg-gray-800 rounded">Inventory</Link>
+          <Link to="/organization-stock" className="p-2 hover:bg-gray-800 rounded">Organization Inventory</Link>
           <Link to="/admin/transfer" className="p-2 hover:bg-gray-800 rounded">Transfers</Link>
           <Link to="/admin/branches" className="p-2 hover:bg-gray-800 rounded">Branches</Link>
           <Link to="/admin/customers" className="p-2 hover:bg-gray-800 rounded">Customers</Link>
