465eb2416d2d2092babf12800002dfec37272490
Nikson Kejeh <niksonkejeh@gmail.com>
2026-03-06 09:45:57 +0100

    Doc Title, added models to admin

--- backend/inventory/admin.py
+++ backend/inventory/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, InventoryBatch
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(InventoryBatch)
+class InventoryBatchAdmin(admin.ModelAdmin):
     pass
--- backend/sales/admin.py
+++ backend/sales/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer, SalesOrder
 
 # Register your models here.
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(SalesOrder)
+class SalesOrderAdmin(admin.ModelAdmin):
     pass
