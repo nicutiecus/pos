653573823d3d9cb50bb03102c948ed9ee755f8ab
Nikson Kejeh <niksonkejeh@gmail.com>
2026-08-05 19:08:11 +0100

    expense table admin

--- backend/finance/admin.py
+++ backend/finance/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Expense
 
 # Register your models here.
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('scope','branch','description','expense_date')
+    list_filter = ('branch','scope')
+    search_fields = ('description',)
+
+admin.site.register(Expense, ExpenseAdmin)
+    
