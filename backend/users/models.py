80eb30e2a593549e61a24c0e7c90489d61f229af
Nikson Kejeh <niksonkejeh@gmail.com>
2026-08-06 14:00:49 +0100

    local docker update

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,9 +1,6 @@
 from django.db import models
-
-# Create your models here.
 import uuid
 from django.contrib.auth.models import AbstractUser, BaseUserManager
-from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 class Tenant(models.Model):
