41ff957f564100323632259918017726660f812e
Nikson Kejeh <niksonkejeh@gmail.com>
2026-08-19 15:17:49 +0100

    removed index.html caching

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,16 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux';
 import { store } from './store/index.ts';
+import {registerSW} from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+     updateSW(true); // Force skipWaiting and activate new SW immediately
+   },
+   onOfflineReady() {
+     console.log('App ready for offline use.');
+   },
+ });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
